b784e3fecdd06c9b0df8a470ce8ee18e6297b4b9
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-02-01 22:57:55 -0300

    🚧 Add HeaderRow and CryptoData Columns

---
+++ src/components/molecules/HeaderCrypto/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import S from "./styles";
+import Text from "../../atoms/Text";
+
+const HeaderCrypto: React.FC = () => {
+  return (
+    <S.Container>
+      <S.CryptoPosition>#</S.CryptoPosition>
+      <Text type={"paragraph_text"}>#</Text>
+      <Text type={"paragraph_text"}>Name</Text>
+      <Text type={"paragraph_text"}>Price</Text>
+      <Text type={"paragraph_text"}>24%</Text>
+      <Text type={"paragraph_text"}>7%</Text>
+      <Text type={"paragraph_text"}>Market Cap</Text>
+      <Text type={"paragraph_text"}>Volume(24h)</Text>
+      <Text type={"paragraph_text"}>Circulating Supply</Text>
+      <Text type={"paragraph_text"}>Last 7 Days</Text>
+    </S.Container>
+  );
+};
+
+export default HeaderCrypto;
---
+++ src/components/molecules/HeaderCrypto/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  margin-top: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+const CryptoPosition = styled.div``;
+
+const CryptoName = styled.div``;
+
+const S = {
+  Container,
+  CryptoPosition,
+  CryptoName,
+};
+
+export default S;
