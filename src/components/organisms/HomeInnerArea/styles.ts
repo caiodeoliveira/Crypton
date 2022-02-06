18e096b3df40643e21b4fadb06113efbbe2b0045
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-02-05 22:49:22 -0300

    Changed header text size

--- src/components/organisms/HomeInnerArea/styles.ts
+++ src/components/organisms/HomeInnerArea/styles.ts
@@ -3,26 +3,39 @@ import bg_big from "../../../assets/bg_big.jpg";
 import { colors } from "../../../styles/mixins";
 
 const BackgroundCrypto = styled.div`
-  background: url(${bg_big}) no-repeat;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  background: url(${bg_big});
+  background-size: cover;
 `;
 
-const CryptoGridContainer = styled.div`
-  width: 80%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+const CryptoFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  gap: 20px;
+`;
+
+const CryptoGridContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   align-items: center;
+  margin-top: 100px;
+`;
+
+const CryptoHeader = styled.header`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
 `;
 
 const CryptoRank = styled.p``;
 
+const CoinContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
 const CryptoImage = styled.img`
-  width: 40px;
+  width: 60px;
 `;
 
 const CryptoPrice = styled.p``;
@@ -39,9 +52,12 @@ const CryptoSupply = styled.p``;
 
 const S = {
   BackgroundCrypto,
+  CryptoFlexContainer,
+  CryptoGridContent,
+  CryptoHeader,
+  CoinContainer,
   CryptoRank,
   CryptoImage,
-  CryptoGridContainer,
   CryptoPrice,
   CryptoDayStatus,
   CryptoWeekStatus,
