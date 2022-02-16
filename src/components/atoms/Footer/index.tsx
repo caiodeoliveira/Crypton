900279ddc0f6ff70b6d4f484d8573b14240216cf
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-02-15 22:52:45 -0300

    Add Pagination Component

--- src/components/atoms/Footer/index.tsx
+++ src/components/atoms/Footer/index.tsx
@@ -1,12 +1,14 @@
 import Text from "../../atoms/Text";
 import S from "./styles";
 import Pagination from "@material-ui/lab/Pagination";
+
 const Footer = () => {
   return (
     <S.Footer>
       <Text type="footer">
         {"Copyright © 2010-2022 CryptoHouse Company  All rights reserved."}
       </Text>
+      <Pagination count={10} />
     </S.Footer>
   );
 };
